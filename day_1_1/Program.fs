d990241dc296142500d7750269988e40714cffa2
admiralfailure <joe.andrews95@hotmail.co.uk>
2025-12-01 23:46:08 +0000

    Day 1

---
+++ day_1_1/Program.fs
@@ -0,0 +1,53 @@
+﻿let minValue = 0
+let maxValue = 99
+
+let constrain (x) = 
+    let change = x % 100
+    if change > maxValue then
+        change - maxValue - 1
+    elif change < minValue then
+        change + maxValue + 1
+    else
+        change
+
+let constrainedAdd (x, y) = 
+    x + y
+    |> constrain
+
+let constrainedSubtract (x, y) =
+    x - y
+    |> constrain
+
+let processInput (startPoint, direction, amount) =
+    match direction with
+        | "L" -> constrainedSubtract (startPoint, amount)
+        | "R" -> constrainedAdd (startPoint, amount)
+        | _ -> raise (System.ArgumentException("This is knackered"))
+
+let parseInputRow (input: string) =
+    let direction = input[0].ToString()
+    let amount= input[1..].ToString() |> int
+
+    direction, amount
+
+let readLines (filePath:string) = seq {
+    use sr = new System.IO.StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
+
+let run (path) =
+    let mutable zeros = 0
+    let mutable current = 50
+    for line in readLines path do
+        let direction, amount = parseInputRow (line)
+        printfn "Processing input %s %d from %d..." direction amount current
+        current <- processInput(current, direction, amount)
+        printfn "%d" current
+
+        if current = 0 then
+            zeros <- zeros + 1
+
+    zeros
+    
+printfn "Number of 0s: %d" (run "input.txt")
---
+++ day_1_2/Program.fs
@@ -0,0 +1,73 @@
+﻿let minValue = 0
+let maxValue = 99
+
+let constrain (x) = 
+    let change = x
+    if change > maxValue then
+        minValue
+    elif change < minValue then
+        maxValue
+    else
+        change
+
+let constrainedAdd (x, y) = 
+    let mutable output = x;
+    let mutable currentZeros = 0
+    for click in 1..y do
+        if output = 0 then
+            currentZeros <- currentZeros + 1
+            printfn "Current 0s: %d" currentZeros
+        output <- constrain (output + 1)
+
+    output, currentZeros
+
+
+let constrainedSubtract (x, y) = 
+    let mutable output = x;
+    let mutable currentZeros = 0
+    for click in 1..y do
+        if output = 0 then
+            currentZeros <- currentZeros + 1
+            printfn "Current 0s: %d" currentZeros
+        output <- constrain (output - 1)
+    
+    output, currentZeros
+
+let processInput (startPoint, direction, amount) =
+    let current = 
+        match direction with
+            | "L" -> constrainedSubtract (startPoint, amount)
+            | "R" -> constrainedAdd (startPoint, amount)
+            | _ -> raise (System.ArgumentException("This is knackered"))
+
+    current
+
+let parseInputRow (input: string) =
+    let direction = input[0].ToString()
+    let amount= input[1..].ToString() |> int
+
+    direction, amount
+
+let readLines (filePath:string) = seq {
+    use sr = new System.IO.StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
+
+let run (path) =
+    let mutable totalZeros = 0
+    let mutable current = 50
+    for line in readLines path do
+        let mutable currentZeros = 0
+        let direction, amount = parseInputRow (line)
+        printfn "Processing input %s %d from %d..." direction amount current
+        let (newCurrent, newCurrentZeros) = processInput(current, direction, amount)
+        current <- newCurrent
+        currentZeros <- newCurrentZeros
+        printfn "%d" current
+
+        totalZeros <- totalZeros + currentZeros
+
+    totalZeros
+    
+printfn "Number of 0s: %d" (run "input.txt")
