0929e2a5ef328bdc7c055fdc5d6aced5f9c67f53
admiralfailure <joe.andrews95@hotmail.co.uk>
2025-12-02 16:54:41 +0000

    Day 2

---
+++ day_2_1/Program.fs
@@ -0,0 +1,29 @@
+﻿let isMatch (input: string) = input[..(input.Length/2)-1] = input[(input.Length/2)..]
+
+let getMatchTotals (range: bigint list) =
+    [range[0]..range[1]]
+    |> Seq.map string
+    |> Seq.filter isMatch
+    |> Seq.map bigint.Parse
+    |> Seq.sum
+
+let readLines (filePath: string) = System.IO.File.ReadLines(filePath)
+
+let parseInputRow (input: string) =
+    input.Split [|','|]
+
+let buildRanges (input: string) =
+    input.Split [|'-'|]
+    |> Seq.map bigint.Parse
+    |> Seq.toList
+
+let run path =
+    readLines path
+    |> Seq.take 1
+    |> Seq.map parseInputRow
+    |> Seq.concat
+    |> Seq.map buildRanges
+    |> Seq.map getMatchTotals
+    |> Seq.sum
+    
+printfn "Input: %A" (run "input.txt")
---
+++ day_2_2/Program.fs
@@ -0,0 +1,45 @@
+﻿let isMatch (input: string, chunks: int) = 
+    if input.Length < chunks then
+        false
+    else 
+        input
+        |> Seq.chunkBySize ((input.Length + 1) / chunks)
+        |> Seq.distinct
+        |> Seq.length = 1
+
+let rec isMatchRecursive (input: string, chunks: int) =
+    if chunks > input.Length then
+        false
+    elif isMatch(input, chunks) then
+        true
+    else
+        isMatchRecursive(input, chunks + 1)
+
+
+let getMatchTotals (range: bigint list) =
+    [range[0]..range[1]]
+    |> Seq.map string
+    |> Seq.filter (fun x -> isMatchRecursive(x, 2))
+    |> Seq.map bigint.Parse
+    |> Seq.sum
+
+let readLines (filePath: string) = System.IO.File.ReadLines(filePath)
+
+let parseInputRow (input: string) =
+    input.Split [|','|]
+
+let buildRanges (input: string) =
+    input.Split [|'-'|]
+    |> Seq.map bigint.Parse
+    |> Seq.toList
+
+let run path =
+    readLines path
+    |> Seq.take 1
+    |> Seq.map parseInputRow
+    |> Seq.concat
+    |> Seq.map buildRanges
+    |> Seq.map getMatchTotals
+    |> Seq.sum
+    
+printfn "Input: %A" (run "input.txt")
