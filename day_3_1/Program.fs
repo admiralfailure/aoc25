87f064eb253498e04c0468ed64993a80cf61af9e
admiralfailure <joe.andrews95@hotmail.co.uk>
2025-12-03 23:59:36 +0000

    Day 3

---
+++ day_3_1/Program.fs
@@ -0,0 +1,19 @@
+﻿let firstOccurrenceOfMaxValue input =
+    Seq.indexed input
+    |> Seq.maxBy snd 
+         
+let maxJoltage (input: string) =
+    firstOccurrenceOfMaxValue input[0..-2]
+    |> fun firstDigit -> seq {
+        [| snd firstDigit; snd (firstOccurrenceOfMaxValue input[fst firstDigit + 1..]) |]
+    }
+    |> Array.concat 
+    |> System.String
+
+let run path =
+    System.IO.File.ReadLines path
+    |> Seq.map maxJoltage
+    |> Seq.map int
+    |> Seq.sum
+
+printfn "Total max joltage: %A" (run "input.txt")
---
+++ day_3_2/Program.fs
@@ -0,0 +1,23 @@
+﻿let firstOccurrenceOfMaxValue input startIndex endIndex =
+    Seq.indexed input
+    |> Seq.filter (fun x -> (fst x) >= startIndex && (fst x) <= endIndex)
+    |> Seq.maxBy snd 
+
+let getBankJoltage digits (bank: string) =
+    [1..digits]
+    |> Seq.rev
+    |> Seq.scan (fun digitOfOutput (index) -> 
+            firstOccurrenceOfMaxValue bank (fst digitOfOutput + 1) (bank.Length - index)
+        ) (-1, ' ')
+    |> Seq.skip 1
+    |> Seq.map snd
+    |> System.String.Concat
+    |> int64
+
+let run path =
+    System.IO.File.ReadLines path
+    |> Seq.map (getBankJoltage 12)
+    |> Seq.sum
+
+
+printfn "Total max joltage: %A" (run "input.txt")
