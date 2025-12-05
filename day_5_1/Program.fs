8c1eaa8a8df4a997bb158e2f9d535d2a3bce5807
admiralfailure <joe.andrews95@hotmail.co.uk>
2025-12-05 18:06:10 +0000

    Day 5

---
+++ day_5_1/Program.fs
@@ -0,0 +1,34 @@
+﻿let parseInput filePath separator =
+    let lines = System.IO.File.ReadAllLines filePath
+    let separatorIdx = Seq.findIndex (fun x -> x = "") lines
+    let freshIDRanges = lines |> Seq.take separatorIdx
+    let availableIDs = lines |> Seq.skip (separatorIdx + 1) |> Seq.map int64
+
+    (freshIDRanges, availableIDs)
+
+let isWithinRange start finish value =
+    value >= start && value <= finish
+
+let processRanges (input: string seq) =
+    input
+    |> Seq.map (fun range -> range.Split '-')
+    |> Seq.map (fun range -> 
+        match range with
+        | [| start; finish |] -> (isWithinRange (start |> int64) (finish |> int64))
+        | _ -> failwith "Could not parse start and finish from range"
+    )
+
+let run filePath =
+    let (freshIDRanges, availableIDs) = parseInput filePath ""
+    let rangeFunctions = processRanges freshIDRanges
+
+    availableIDs
+    |> Seq.map (fun id ->
+        rangeFunctions
+        |> Seq.exists (fun rangeFunction -> rangeFunction id)
+    )
+    |> Seq.filter (fun result -> result)
+    |> Seq.length
+
+    
+printfn "Result: %A" (run "input.txt")
---
+++ day_5_2/Program.fs
@@ -0,0 +1,50 @@
+﻿let parseInput filePath separator =
+    let lines = System.IO.File.ReadAllLines filePath
+    let separatorIdx = Seq.findIndex (fun x -> x = "") lines
+    let freshIDRanges = lines |> Seq.take separatorIdx
+    let availableIDs = lines |> Seq.skip (separatorIdx + 1) |> Seq.map int64
+
+    (freshIDRanges, availableIDs)
+
+let processRanges (input: string seq) =
+    input
+    |> Seq.map (fun range -> range.Split '-')
+    |> Seq.map (fun range -> 
+        match range with
+        | [| start; finish |] -> 
+            ((start |> int64), (finish |> int64))
+        | _ -> failwith "Could not parse range"
+    )
+    |> Seq.toList
+
+let combineRanges ranges =
+    let rec combine (ranges: (int64 * int64) list) idx =
+        if idx = (List.length ranges) - 1 then
+            ranges
+        else
+            let (range_0_start, range_0_finish) = ranges[idx]
+            let (range_1_start, range_1_finish) = ranges[idx + 1]
+
+            if range_0_finish >= range_1_start then
+                let new_range_start = range_0_start
+                let new_range_finish = max range_0_finish range_1_finish
+
+                combine (ranges[..idx-1] @ (new_range_start, new_range_finish) :: ranges[idx+2..]) idx 
+            else
+                combine ranges (idx + 1)
+
+    combine ranges 0
+
+let run filePath =
+    let (freshIDRanges, _) = parseInput filePath ""
+    let ranges = processRanges freshIDRanges
+
+    ranges
+    |> List.sort
+    |> combineRanges
+    |> Seq.map (fun (start, finish) -> (finish + 1L) - start)
+    |> Seq.sum
+
+
+    
+printfn "Result: %A" (run "input.txt")
