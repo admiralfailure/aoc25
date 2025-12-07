9aebf1442ab0ee1e76765ec5972058c8ca1d03b7
admiralfailure <joe.andrews95@hotmail.co.uk>
2025-12-07 01:18:28 +0000

    Day 6

---
+++ day_6_1/Program.fs
@@ -0,0 +1,31 @@
+﻿let parseInput filePath =
+    let input = System.IO.File.ReadAllLines filePath
+    match input with
+    | [| numbers_0; numbers_1; numbers_2; numbers_3; operators |] -> 
+        let numbersArray_0 = numbers_0.Split ' ' |> Array.filter (fun x -> x <> "") |> Array.map int64
+        let numbersArray_1 = numbers_1.Split ' ' |> Array.filter (fun x -> x <> "") |> Array.map int64
+        let numbersArray_2 = numbers_2.Split ' ' |> Array.filter (fun x -> x <> "") |> Array.map int64
+        let numbersArray_3 = numbers_3.Split ' ' |> Array.filter (fun x -> x <> "") |> Array.map int64
+        let operatorsArray = operators.Split ' ' |> Array.filter (fun x -> x <> "")
+
+        (numbersArray_0, numbersArray_1, numbersArray_2, numbersArray_3, operatorsArray)
+    | _ -> failwith "Unable to parse input"
+
+let performOperation number_0 number_1 number_2 number_3 operator =
+    match operator with
+    | "+" -> number_0 + number_1 + number_2 + number_3
+    | "*" -> number_0 * number_1 * number_2 * number_3
+    |_ -> failwith "Unknown operator"
+
+let run filePath =
+    let numbersArray_0, numbersArray_1, numbersArray_2, numbersArray_3, operatorsArray = parseInput filePath
+
+    operatorsArray
+    |> Array.mapi (fun idx operator ->
+        performOperation numbersArray_0[idx] numbersArray_1[idx] numbersArray_2[idx] numbersArray_3[idx] operator
+    )
+    |> Array.sum
+
+
+printfn "Result: %A" (run "input.txt")
+    
---
+++ day_6_2/Program.fs
@@ -0,0 +1,99 @@
+﻿let getSplitPoints operators = 
+    let (results, idx) =
+        operators
+        |> Seq.toArray
+        |> Array.fold(fun (results, idx) current ->
+            match current with
+            | ' ' -> (results, idx + 1)
+            | _ -> (idx :: results, idx + 1)
+        ) ([], 0)
+
+    List.rev (results)
+    |> List.skip 1
+
+let splitAtPoints (splitPoints: int list) (input: string) =
+    let maxIdx = splitPoints.Length - 1
+
+    let parts = 
+        splitPoints
+        |> List.indexed
+        |> List.fold(fun result (idx, point) ->
+            match idx with
+            | 0 -> input[0..point-2] :: result
+            | _ -> input[splitPoints[idx-1]..point-2] :: result
+        ) [""]
+    
+    List.rev (input[splitPoints[maxIdx]..] :: parts)
+    |> List.skip 1
+
+let parseInput filePath =
+    let input = System.IO.File.ReadAllLines filePath
+
+    // Last line is the operators
+    let operators = input[input.Length - 1]
+
+    // Get the points to split the strings
+    let splitPoints = getSplitPoints operators
+
+    // Split the rows
+    let numbers =
+        input
+        |> Array.take (input.Length - 1)
+        |> Array.map (splitAtPoints splitPoints) 
+        |> Array.toList
+
+    // Clean up the operators and prepend them
+    (operators.Split ' ' |> Array.filter (fun x -> x <> "") |> Array.toList) :: numbers
+
+let readNumberFromColumn (input: string list) =
+    input[0]
+    |> Seq.toArray
+    |> Array.indexed
+    |> Array.fold(fun result (idx, value) ->
+        let column = 
+            input
+            |> List.map(fun item -> item[idx])
+            |> System.String.Concat
+
+        column :: result
+    ) []
+    |> List.map int64
+    
+let performOperation (numbers: int64 list) operator =
+    match operator with
+    | "+" -> List.sum numbers
+    | "*" -> List.fold (fun acc cur -> acc * cur) (1) numbers
+    |_ -> failwith "Unknown operator"
+
+let processInput (input: string list list) =
+    let operators = 
+        input
+        |> List.take 1
+        |> List.concat
+
+    let numbers =
+        input
+        |> List.skip 1
+
+    let rotated = 
+        [0..(operators.Length - 1)]
+        |> List.map(fun idx ->
+            numbers
+            |> List.map (fun items -> items[idx])
+        )
+
+    rotated
+    |> List.map readNumberFromColumn
+    |> List.mapi(fun idx numbers ->
+        performOperation numbers operators[idx]
+    )
+    |> List.sum
+
+    
+let run filePath =
+    parseInput filePath
+    |> processInput
+
+    
+printfn "Result: %A" (run "input.txt")
+    
